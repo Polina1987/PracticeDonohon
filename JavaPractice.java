3425be377b81abd4a1d180892b585c7b9752ce30
Poli Blintsovskaya <pblintso@gmail.com>
2020-06-17 10:46:58 -0400

    pushing more java code into git hub

---
+++ JavaPractice.java
@@ -0,0 +1,7 @@
+public class JavaPractice {
+public static void main(String[] args) {
+System.out.println("The entries are: " + args[0] + " - " + args[1] + " - " + args[2] + " - " + args[3] + " - " + args[4]);
+
+}
+
+}
