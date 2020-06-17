95287c7e5caabedfad2556ffdf5584a202ec043e
Poli Blintsovskaya <pblintso@gmail.com>
2020-06-17 11:26:48 -0400

    made changes to August 21 source code

--- JavaPractice.java
+++ JavaPractice.java
@@ -1,6 +1,6 @@
 public class JavaPractice {
 public static void main(String[] args) {
-System.out.println("The entries are: " + args[0] + " - " + args[1] + " - " + args[2] + " - " + args[3] + " - " + args[4]);
+System.out.println("The entries are: " + args[0] + " - " + args[1] + " - " + args[2] + " - " + args[3] + " - " + args[4] + " - " + args[5]);
 
 }
 
