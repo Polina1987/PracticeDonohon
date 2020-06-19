573c2c2a872fa7c1cdfecb1e5c9131702e18c54e
Poli Blintsovskaya <pblintso@gmail.com>
2020-06-18 23:00:20 -0400

    June 18 edit - remove arg[5]

--- JavaPractice.java
+++ JavaPractice.java
@@ -1,7 +1,7 @@
 public class JavaPractice {
 public static void main(String[] args) {
 //Donna and I are collaborating on this project.
-System.out.println("The entries are: " + args[0] + " - " + args[1] + " - " + args[2] + " - " + args[3] + " - " + args[4] + " - " + args[5]);
+System.out.println("The entries are: " + args[0] + " - " + args[1] + " - " + args[2] + " - " + args[3] + " - " + args[4]);
 
 }
 
