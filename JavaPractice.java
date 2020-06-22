9aa1bd523b485ef51460b9e65cb7141c4047ac61
donohon <54605678+donna115@users.noreply.github.com>
2020-06-21 22:12:26 -0400

    Update JavaPractice.java

--- JavaPractice.java
+++ JavaPractice.java
@@ -2,7 +2,7 @@ public class JavaPractice {
 public static void main(String[] args) {
 //Donna and I are collaborating on this project.
 System.out.println("The entries are: " + args[0] + " - " + args[1] + " - " + args[2] + " - " + args[3] + " - " + args[4]);
-
+System.out.println( " Thank you so much Polina for Study Sessions")
 }
 
 }
